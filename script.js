63363b71e62484a356b69f4e70c96f88a5235d34
Miyami Kuro <jeromejjcho@gmail.com>
2021-02-16 20:52:48 -0800

    Added revolutionary website files

---
+++ script.js
@@ -0,0 +1,9 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {// can be whatever, 0 refers to the top space you allow
+        $('.move').hide();// Hide your element
+        console.log("Works!");
+    }
+    else {
+        $('.move').show();// It's just if you want to show back the element if we're back on top
+    }
+});
